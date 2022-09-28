846d79d89262cb68c4ad2592a13c1686689e7d66
sh1eldma1den <@pOOsa856100>
2022-09-28 10:40:29 -0400

    trying to fix post

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
-router.use('/painters', require('./painters'));
+router.use('/', require('./painters'));
 router.use('/',
     (docData = (req,res) => {
         let docData = {

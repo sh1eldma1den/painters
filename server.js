9382251edeb0e3647f1a772412022b8f5b65a472
sh1eldma1den <@pOOsa856100>
2022-09-30 11:17:10 -0400

    hopeful

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-//     );
-//     res.setHeader('Content-Type', 'application/json');
-//     res.setHeader('Access-Control--Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control--Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));

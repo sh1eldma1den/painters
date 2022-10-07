92b6165ffac614fcfaf57bc230cb7490eb247c6e
sh1eldma1den <@pOOsa856100>
2022-10-06 20:05:49 -0400

    maybe

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,5 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/customers', require('./customers'));
 router.use('/classes', require('./classes'));
-// router.use('/',
-//     (docData = (req,res) => {
-//         let docData = {
-//             documentationURL:'https://github.com/sh1eldma1den/painters/tree/main/api-docs',
-//         };
-//         res.send(docData);
-//     })
-// );
+
 module.exports = router;

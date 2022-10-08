1e88d8391052e07af1fc3ee160b28984f2c16505
sh1eldma1den <@pOOsa856100>
2022-10-08 12:36:34 -0400

    login/logout/authorization

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const { auth, requiresAuth } = require('express-openid-connect');
 require('dotenv').config();
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(),(req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
 
 const port = process.env.PORT || 8000;
 
@@ -40,26 +59,6 @@ process.on('uncaughtException', (err, origin) => {
   });
 
 app.use('/', require('./routes'));
-
-// const {validationResult, check} = require('express-validator');
-// const {
-//     createCustomerValidation,
-//     createClassValidation,
-//     viewOneCustomerValidation,
-//     viewOneClassValidation,
-//     deleteOneCustomerValidation,
-//     deleteOneClassValidation
-// }  = require('./checks/validation.js')(check);
-
-// const dependencies = {
-//     validationResult: validationResult,
-//     createCustomerValidation: createCustomerValidation,
-//     createClassValidation: createClassValidation,
-//     viewOneCustomerValidation: viewOneCustomerValidation,
-//     viewOneClassValidation: viewOneClassValidation,
-//     deleteOneCustomerValidation: deleteOneCustomerValidation,
-//     deleteOneClassValidation: deleteOneClassValidation
-// };
 
 mongodb.initDb((err, mongodb) => {
     if (err) {

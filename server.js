a8c6a9f00f5bda0d0ece0d4d71072088b637f061
sh1eldma1den <@pOOsa856100>
2022-10-08 20:11:35 -0400

    authorization correction, fix failed render deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const config = {
   secret: process.env.SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
 
 app.use(auth(config));

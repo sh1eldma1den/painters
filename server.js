30350b424b5eaa6c81838a54c314aa6c646cb195
sh1eldma1den <@pOOsa856100>
2022-10-10 11:09:18 -0400

    possible changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const config = {
   secret: process.env.SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
 };
 
 
